c5354d8cadfbbf8be7afbc1ce42898915979a555
JoelJansenD <joeljansend@gmail.com>
2026-07-15 20:19:47 +0200

    Added pending steps

---
+++ test/e2e/steps/terrain.steps.ts
@@ -0,0 +1,21 @@
+import { Given, When, Then } from '@wdio/cucumber-framework';
+
+Given('I have selected the brush tool', async function () {
+    return 'pending';
+});
+
+Given('I have selected the terrain layer', async function () {
+    return 'pending';
+});
+
+Given('my selected colour is blue', async function () {
+    return 'pending';
+});
+
+When('I click an empty hex', async function () {
+    return 'pending';
+});
+
+Then('the hex terrain will be painted blue', async function () {
+    return 'pending';
+});
