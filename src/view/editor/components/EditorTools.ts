0c3b65bc1deb6aa3d1cfdfa56a18821c3296a7bb
JoelJansenD <joeljansend@gmail.com>
2026-07-16 06:24:28 +0200

    Styling

--- src/view/editor/components/EditorTools.ts
+++ src/view/editor/components/EditorTools.ts
@@ -1,3 +1,5 @@
+import { Brush, createElement, Eraser, MousePointer, PaintBucket } from "lucide";
+
 export default class EditorTools {
     constructor(private _parentEl: HTMLElement) {
         this.build();
@@ -5,5 +7,17 @@ export default class EditorTools {
 
     private build() {
         const toolsEl = this._parentEl.createEl('div', { cls: 'hexer-tools' });
+
+        const selectButton = toolsEl.createEl('div', { cls: 'hexer-tools-button' });
+        selectButton.appendChild(createElement(MousePointer, { width: 18, height: 18 }));
+
+        const brushButton = toolsEl.createEl('div', { cls: 'hexer-tools-button' });
+        brushButton.appendChild(createElement(Brush, { width: 18, height: 18 }));
+
+        const bucketButton = toolsEl.createEl('div', { cls: 'hexer-tools-button' });
+        bucketButton.appendChild(createElement(PaintBucket, { width: 18, height: 18 }));
+
+        const eraseButton = toolsEl.createEl('div', { cls: 'hexer-tools-button' });
+        eraseButton.appendChild(createElement(Eraser, { width: 18, height: 18 }));
     }
 }
