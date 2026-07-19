a4b4b45fb4d99261bce83d098630f8b1af6616b8
JoelJansenD <joeljansend@gmail.com>
2026-07-19 16:52:26 +0200

    Overwrite hex

--- test/e2e/steps/terrain.steps.ts
+++ test/e2e/steps/terrain.steps.ts
@@ -3,6 +3,7 @@ import { expect } from '@wdio/globals';
 import { clickHex, getHex, selectLayer, selectPaintTool, setTerrainColour } from '../support/editor.page';
 
 const EMPTY_HEX = { q: 1, r: 1 };
+let lastClickedHex: { q: number, r: number } | null = null;
 
 Given('I have selected the brush tool', async function () {
     await selectPaintTool('brush');
@@ -18,9 +19,16 @@ Given('my selected colour is blue', async function () {
 
 When('I click an empty hex', async function () {
     await clickHex(EMPTY_HEX);
+    lastClickedHex = EMPTY_HEX;
+});
+
+When('I click a hex with coloured terrain', async function () {
+    await clickHex({ q: 2, r: 2 });
+    lastClickedHex = { q: 2, r: 2 };
 });
 
 Then('the hex terrain will be painted blue', async function () {
-    const hex = await getHex(EMPTY_HEX);
+    expect(lastClickedHex).not.toBeNull();
+    const hex = await getHex(lastClickedHex!);
     expect(hex?.terrainColor).toBe('#0000ff');
 });
