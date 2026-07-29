41581592b0f7bf191800f70a8e2156bdfe1e8790
JoelJansenD <joeljansend@gmail.com>
2026-07-29 11:32:49 +0200

    Add node on click

--- src/logic/toolStrategies/PathPolygonStrategy.test.ts
+++ src/logic/toolStrategies/PathPolygonStrategy.test.ts
@@ -1,19 +1,58 @@
+import createHexerData from "../../__test/createHexerData";
+import defaultEditorState from "../../__test/defaultEditorState";
+import { HexerData, hexKey } from "../HexerData";
+import { Path } from "../path";
 import PathPolygonStrategy from "./PathPolygonStrategy";
-
-describe('canBeApplied', () => {
-
-    let strategyToTest: PathPolygonStrategy;
-    beforeEach(() => {
-        strategyToTest = new PathPolygonStrategy();
-    });
-
-});
 
 describe('onLeftClick', () => {
 
+    let defaultHexerData: HexerData;
+    let targetPath: Path;
     let strategyToTest: PathPolygonStrategy;
+    
     beforeEach(() => {
+        targetPath = new Path('Empty river');
+        defaultHexerData = createHexerData();
+        defaultHexerData.rivers.push(targetPath);
+
         strategyToTest = new PathPolygonStrategy();
+    });
+
+    it('does not make changes to a path when no path is active', () => {
+        // Arrange
+        const editorState = {...defaultEditorState, activePath: null};
+        const data = defaultHexerData.clone();
+
+        // Act
+        strategyToTest.onLeftClick(data, editorState, {q: 0, r: 0});
+
+        // Assert
+        expect(data).toEqual(defaultHexerData);
+    });
+
+    it('throws an exception if the active path is not found in the data', () => {
+        // Arrange
+        const editorState = {...defaultEditorState, activePath: new Path('Nonexistent path')};
+        const data = defaultHexerData.clone();
+
+        // Act & Assert
+        expect(() => {
+            strategyToTest.onLeftClick(data, editorState, {q: 0, r: 0});
+        }).toThrow(`Active path 'Nonexistent path' with id ${editorState.activePath.id} not found in data.`);
+    });
+
+    it('adds a node to the active path when a path is active', () => {
+        // Arrange
+        const editorState = {...defaultEditorState, activePath: targetPath};
+        const data = defaultHexerData.clone();
+
+        // Act
+        strategyToTest.onLeftClick(data, editorState, {q: 0, r: 0});
+
+        // Assert
+        const river = data.rivers[0];
+        expect(river.nodes.size).toBe(1);
+        expect(river.nodes.has('0,0')).toBe(true);
     });
 
 });
--- src/logic/toolStrategies/PathPolygonStrategy.ts
+++ src/logic/toolStrategies/PathPolygonStrategy.ts
@@ -1,6 +1,7 @@
 import { EditorState, Layer, PaintTool } from "../EditorState";
 import { RadialCoordinates } from "../hexagon";
 import { HexerData, HexMap } from "../HexerData";
+import { Path } from "../path";
 import { RegisteredEvents, ToolStrategy } from "./ToolStrategy";
 
 export default class PathPolygonStrategy implements ToolStrategy {
@@ -9,6 +10,21 @@ export default class PathPolygonStrategy implements ToolStrategy {
     }
 
     public onLeftClick(data: HexerData, editorState: EditorState, radialCoordinates: RadialCoordinates) {
+        if(!editorState.activePath) {
+            return;
+        }
+
+        const allPaths = [...data.rivers, ...data.roads];
+        const targetPath = this.getPath(editorState.activePath.id, allPaths);
+        if(!targetPath) {
+            throw new Error(`Active path '${editorState.activePath.name}' with id ${editorState.activePath.id} not found in data.`);
+        }
+
+        targetPath.addNode(radialCoordinates);
+    }
+
+    private getPath(id: string, paths: Path[]): Path | null {
+        return paths.find(path => path.id === id) || null;
     }
 
     public getEvents(): RegisteredEvents {
